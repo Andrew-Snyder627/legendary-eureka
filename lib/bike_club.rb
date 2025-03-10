e362b06bdffe05fdc76c353c39d901599d1de578
Andrew Snyder <andrew.snyder9702@gmail.com>
2025-03-10 12:52:32 -0400

    Creates most_rides method and passes test

--- lib/bike_club.rb
+++ lib/bike_club.rb
@@ -1,3 +1,24 @@
 class BikeClub
+  attr_reader :name, :bikers
 
+  def initialize(name)
+    @name = name
+    @bikers = []
+  end
+
+  def add_biker(biker)
+    @bikers << biker
+  end
+
+  def most_rides
+    @bikers.max_by do |biker|
+      biker.rides.values.sum do |times|
+        times.count
+      end
+    end
+  end
+
+  def best_time(ride)
+    @biker
+  end
 end

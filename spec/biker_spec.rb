12855e52904d53d17da6af4ad8c5bd96d126bad9
Andrew Snyder <andrew.snyder9702@gmail.com>
2025-03-10 11:52:38 -0400

    Finishes tests for iteration 2

--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -41,26 +41,56 @@ RSpec.describe Biker do
 
   describe '#log_ride' do
     it 'logs rides if terrain and distance are acceptable' do
-      @biker.log_ride(ride1, 92.5)
-      @biker.log_ride(ride1, 91.1)
-      @biker.log_ride(ride2, 60.9)
-      @biker.log_ride(ride2, 61.6)
+      @biker.learn_terrain(:gravel)
+      @biker.learn_terrain(:hills)
+
+      @biker.log_ride(@ride1, 92.5)
+      @biker.log_ride(@ride1, 91.1)
+      @biker.log_ride(@ride2, 60.9)
+      @biker.log_ride(@ride2, 61.6)
 
       expected = {
-        ride1 => [92.5, 91.1]
-        ride2 => [60.9, 61.6]
+        @ride1 => [92.5, 91.1]
+        @ride2 => [60.9, 61.6]
       }
 
       expect(@biker.rides).to eq(expected)
     end
 
     it 'does not log rides if terrain is not known' do
-      @biker2.log_ride(ride1, 95.0) #too long
-      @biker2.log_ride(ride2, 65.0) #Should pass
+      @biker2.log_ride(@ride1, 97.0)
+      @biker2.log_ride(@ride2, 67.0)
 
-      expect(biker2.rides).to eq({
-        ride2 => [65.0]
-      })
+      expect(@biker2.rides).to eq({})
+    end
+
+    it 'does not log rides if distance is too long' do
+      @biker2.learn_terrain(:gravel)
+      @biker2.learn_terrain(:hills)
+
+      @biker2.log_ride(@ride1, 95.0) #too long
+      @biker2.log_ride(@ride2, 65.0) #Should work
+
+      expect(@biker2.rides).to eq({@ride2 => [65.0]})
+    end
+  end
+
+  describe '#personal_record' do
+    it 'returns personal record for a ride' do
+      @biker.learn_terrain(:gravel)
+      @biker.learn_terrain(:hills)
+
+      @biker.log_ride(@ride1, 92.5)
+      @biker.log_ride(@ride1, 91.1)
+      @biker.log_ride(@ride2, 60.9)
+      @biker.log_ride(@ride2, 61.6)
+
+      expect(@biker.personal_record(@ride1)).to eq(91.1)
+      expect(@biker.personal_record(@ride2)).to eq(60.9)
+    end
+
+    it 'returns false if the biker has not completed a ride' do
+      expect(biker2.personal_record(@ride1)).to eq(false)
     end
   end
 end
